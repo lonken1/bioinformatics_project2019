d91adfe9b7d29e6488ebae37d7194b36a64dcf26
Luke Onken <lonken@crcfe02.crc.nd.edu>
2019-10-15 19:49:58 -0400

    Aligned the reference sequences

--- ProjectFlex.sh
+++ ProjectFlex.sh
@@ -1,3 +1,11 @@
 # This is the bioinformatics mid-semester project for Schwarz and Onken
 
-# Create file with all hsp7
+# Create file with all hsp70gene_...fasta sequences and create a file with all the sequences
+cat ./ref_sequences/hsp70gene_*.fasta > ./ref_sequences/allhsp70.fasta
+
+# Do same thing with mcrAgene_...fasta
+cat ./ref_sequences/mcrAgene_*.fasta > ./ref_sequences/allmcrA.fasta
+
+# Run both of these new files through muscle
+~/Private/functions/muscle -in ./ref_sequences/allhsp70.fasta -out ./ref_sequences/hsp70aligned.fasta
+~/Private/functions/muscle -in ./ref_sequences/allmcrA.fasta -out ./ref_sequences/mcrAaligned.fasta  
