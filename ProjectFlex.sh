e780f29cee8a5fc911295a0971268e959a86363d
Luke Onken <lonken@crcfe02.crc.nd.edu>
2019-10-15 20:04:09 -0400

    Built the HMM files for the two sequences

--- ProjectFlex.sh
+++ ProjectFlex.sh
@@ -6,6 +6,10 @@ cat ./ref_sequences/hsp70gene_*.fasta > ./ref_sequences/allhsp70.fasta
 # Do same thing with mcrAgene_...fasta
 cat ./ref_sequences/mcrAgene_*.fasta > ./ref_sequences/allmcrA.fasta
 
-# Run both of these new files through muscle
+# Run both of these new files through muscle to create a sequence alignment
 ~/Private/functions/muscle -in ./ref_sequences/allhsp70.fasta -out ./ref_sequences/hsp70aligned.fasta
 ~/Private/functions/muscle -in ./ref_sequences/allmcrA.fasta -out ./ref_sequences/mcrAaligned.fasta  
+
+# Generate HMM files for both of the aligned sequence files utilizing hmmbuild
+~/Private/functions/hmmer-3.2.1/bin/hmmbuild ./hsp70hmm.fasta ./ref_sequences/hsp70aligned.fasta
+~/Private/functions/hmmer-3.2.1/bin/hmmbuild ./mcrAhmm.fasta ./ref_sequences/mcrAaligned.fasta  
